03e9f0732dd50273910cdbc46aa0abea91b465f4
McMyers411 <nilegend@gmail.com>
2016-10-13 19:14:29 -0400

    Second Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int luck = (int)(Math.random() * 100000 + 1);
+        int luck = (int)(Math.random() * 1000 + 1);
         System.out.println("Howdy, what's your name?");
         String username = input.nextLine().replaceAll("\\s+","");
         System.out.println("Well then " + username + ", your lucky number is " + luck);
